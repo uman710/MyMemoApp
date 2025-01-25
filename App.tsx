924a8f85a03e5a770540fd7bfb41c963ace4ec57
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2025-01-25 23:26:46 +0900

    Helloコンポーネントを導入する

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Hello from "./src/components/Hello";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello>World</Hello>
+      <Hello bang={true} style={{ fontSize: 16 }}>World</Hello>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
