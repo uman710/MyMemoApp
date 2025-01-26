c23cb4e89e4f52fc40e0580b67533f54368ff178
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2025-01-26 23:16:25 +0900

    メモの新規作成および編集画面を作成する

---
+++ src/app/memo/edit.tsx
@@ -0,0 +1,40 @@
+import {KeyboardAvoidingView, StyleSheet, TextInput, View} from "react-native";
+import Header from "../../components/Header";
+import CircleButton from "../../components/CircleButton";
+import Icon from "../../components/icon";
+
+const Edit = () => {
+  return (
+    <KeyboardAvoidingView behavior='height' style={styles.container}>
+      <Header />
+      <View style={styles.inputContainer}>
+        <TextInput multiline={true} style={styles.input} value={'買い物リスト\nリスト'} />
+      </View>
+      <CircleButton>
+        <Icon name='check' size={40} color='#ffffff' />
+      </CircleButton>
+    </KeyboardAvoidingView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  inputContainer: {
+    flex: 1,
+    paddingVertical: 32,
+    paddingHorizontal: 27,
+  },
+  input: {
+    flex: 1,
+    // テキストを上に上げる
+    // androidは下記でOKだが、iosではタグにmultilineを設定しないとだめ
+    textAlignVertical: 'top',
+    fontSize: 16,
+    lineHeight: 24,
+  }
+})
+
+export default Edit
