c9c5dfb8c4899657f698e4d445751b19c20c24d7
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2025-01-26 14:26:43 +0900

    メインページ(index.tsx)の原型を作成する

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,61 @@
-import {View, Text} from "react-native";
+import {View, Text, StyleSheet} from "react-native";
 
 const Index = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <View>
+        <View>
+          <Text>MemoApp</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+      <View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025/01/26 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025/01/26 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025/01/26 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+      </View>
+
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+})
+
 export default Index
