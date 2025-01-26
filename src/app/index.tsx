58821ffffe61a9ac410bb77fddebf9467f8359a9
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2025-01-26 14:39:53 +0900

    ヘッダーのスタイルを作成する

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,12 +3,14 @@ import {View, Text, StyleSheet} from "react-native";
 const Index = () => {
   return (
     <View style={styles.container}>
-      <View>
-        <View>
-          <Text>MemoApp</Text>
-          <Text>ログアウト</Text>
+
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>MemoApp</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
+
       <View>
 
         <View>
@@ -52,9 +54,31 @@ const Index = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
+    flex: 1, //画面いっぱいに広げる
+    backgroundColor: "#ffffff",
+  },
+
+  header: {
+    backgroundColor: "#467FD3",
+    height: 104,
+    justifyContent: "flex-end",
+  },
+
+  headerInner: {
     alignItems: "center",
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: "bold",
+    color: "#ffffff",
+  },
+  headerRight: {
+    position: "absolute",
+    right: 16,
+    bottom: 16,
+    color: 'rgba(255, 255, 255, 0.7)',
   }
 })
 
