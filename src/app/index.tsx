269cd72d119ba87ca03f062e8ce7a013c6130534
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2025-01-26 17:23:28 +0900

    メモリストアイテムのスタイルを適用する

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -13,30 +13,30 @@ const Index = () => {
 
       <View>
 
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2025/01/26 10:00</Text>
+            <Text style={styles.memoListItemTitle} >買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2025/01/26 10:00</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
 
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2025/01/26 10:00</Text>
+            <Text style={styles.memoListItemTitle} >買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2025/01/26 10:00</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
 
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2025/01/26 10:00</Text>
+            <Text style={styles.memoListItemTitle} >買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2025/01/26 10:00</Text>
           </View>
           <View>
             <Text>X</Text>
@@ -45,8 +45,8 @@ const Index = () => {
 
       </View>
 
-      <View>
-        <Text>+</Text>
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
       </View>
     </View>
   )
@@ -79,6 +79,52 @@ const styles = StyleSheet.create({
     right: 16,
     bottom: 16,
     color: 'rgba(255, 255, 255, 0.7)',
+  },
+
+  memoListItem: {
+    backgroundColor: "#ffffff",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderColor: "rgba(0, 0, 0, 0.15)",
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: "#848484",
+  },
+
+  circleButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: "#467DF3",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: 40,
+    bottom: 40,
+
+    // iosにしか適用できないshadow設定
+    shadowColor: "#000000",
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 8 },
+
+    // androidにしか適用できないshadow設定
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: "#ffffff",
+    fontSize: 40,
+    lineHeight: 48,
   }
 })
 
